3c9949fe3ee9b7ded94e2b64f8dbd8d69cac4f07
apiepho <alpiepho@gmail.com>
2022-04-03 18:21:14 -0600

    update gh-pages paths

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,12 +34,12 @@ const RESOURCES = {
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = [
-  "/",
-"main.dart.js",
-"index.html",
-"assets/NOTICES",
-"assets/AssetManifest.json",
-"assets/FontManifest.json"];
+  "https://alpiepho.github.io/flutter_drawing_app/",
+"https://alpiepho.github.io/flutter_drawing_app/main.dart.js",
+"https://alpiepho.github.io/flutter_drawing_app/index.html",
+"https://alpiepho.github.io/flutter_drawing_app/assets/NOTICES",
+"https://alpiepho.github.io/flutter_drawing_app/assets/AssetManifest.json",
+"https://alpiepho.github.io/flutter_drawing_app/assets/FontManifest.json"];
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
   self.skipWaiting();
